557db5e24e78570c6c249e3523fc104f29c997ce
AKCord <shrestha.aeniv@gmail.com>
2025-01-13 12:32:06 +0545

    Add redirects to /support, github, and discord.

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const path = require("path");
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.use(helmet());
+app.use(helmet({ contentSecurityPolicy: false }));
 app.use(compression());
 app.use(express.static(path.join(__dirname, "public")));
 
@@ -14,12 +14,16 @@ app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "public/index.html"));
 });
 
-app.get("/about", (req, res) => {
-  res.sendFile(path.join(__dirname, "public/about.html"));
+app.get("/support", (req, res) => {
+  res.sendFile(path.join(__dirname, "public/support.html"));
 });
 
-app.get("/projects", (req, res) => {
-  res.sendFile(path.join(__dirname, "public/projects.html"));
+app.get("/github", (req, res) => {
+  res.redirect("https://github.com/AKCord");
+});
+
+app.get("/discord", (req, res) => {
+  res.redirect("https://discord.com/users/827062335620382751");
 });
 
 app.use((req, res, next) => {
@@ -27,6 +31,7 @@ app.use((req, res, next) => {
 });
 
 app.use((err, req, res, next) => {
+  console.error(err.stack);
   res.status(500).sendFile(path.join(__dirname, "public/505.html"));
 });
 
